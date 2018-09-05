63394d093aade8bf14853e5a9150ae818633a136
diego fontecilla <mail@diegofontecilla.com>
2018-09-05 17:49:03 +0100

    The user can choose an option

--- app.rb
+++ app.rb
@@ -1,13 +1,19 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_name = params[:player_name]
+    session[:player_name] = params[:player_name]
     erb :play
+  end
+
+  post '/winner' do
+    session[:player_option] = params[:player_option]
+    erb :winner
   end
 end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,6 +1,6 @@
-# feature 'Homepage' do
-#   scenario 'checking homepage message' do
-#     visit('/')
-#     expect(page).to have_content 'Testing infrastructure working!'
-#   end
-# end
+feature 'Homepage' do
+  scenario 'checking homepage message' do
+    visit('/')
+    expect(page).to have_content 'Rock-Paper-Scissor'
+  end
+end
