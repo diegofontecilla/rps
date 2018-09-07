0d324e05c4b33b07930a32a3538379facfa7355b
diego fontecilla <mail@diegofontecilla.com>
2018-09-07 11:56:51 +0100

    Added test for computer.play: returns rock and returns scissors

--- spec/the_computer_spec.rb
+++ spec/the_computer_spec.rb
@@ -5,8 +5,21 @@ describe TheComputer do
   let(:fake_option) { double()}
   subject(:the_computer) { TheComputer.new }
 
-  it 'returns a random option for rock, paper, scissors' do
-    allow(the_computer).to receive(:rand).and_return(1)
-    expect(the_computer.play).to eq('paper')
+  context '#play' do
+
+    it 'returns paper' do
+      allow(the_computer).to receive(:rand).and_return(1)
+      expect(the_computer.play).to eq('paper')
+    end
+
+    it 'returns rock' do
+      allow(the_computer).to receive(:rand).and_return(0)
+      expect(the_computer.play).to eq('rock')
+    end
+
+    it 'returns scissors' do
+      allow(the_computer).to receive(:rand).and_return(2)
+      expect(the_computer.play).to eq('scissors')
+    end
   end
 end
