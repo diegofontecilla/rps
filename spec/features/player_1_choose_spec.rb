b72a10ed4de25df046b462694c123dbc1f36444f
diego fontecilla <mail@diegofontecilla.com>
2018-09-19 14:03:23 +0100

    Feature test enter two names green

--- spec/features/player_1_choose_spec.rb
+++ spec/features/player_1_choose_spec.rb
@@ -1,12 +1,11 @@
-# feature 'Player 1 choose' do
-#   scenario '1 of the options' do
-#     visit('/')
-#     fill_in :player_1_name, with: 'Diego'
-#     click_button 'Submit'
-#     visit('/play')
-#     find('[name=player_1_option, value=rock]').click
-#     click_button 'Submit'
-#     visit('/winner')
-#     expect(page).to have_content 'Diego chose rock'
-#   end
-# end
+feature 'Player 1 choose' do
+  scenario '1 of the options' do
+    visit('/')
+    fill_in :player_1_name, with: 'Diego'
+    click_button 'Submit'
+    choose('rock')
+    click_button 'Submit'
+    visit('/winner')
+    expect(page).to have_content 'Diego chose rock'
+  end
+end
